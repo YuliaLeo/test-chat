0021a6ddb137e72a684c3419b866771e92b335cf
Yulia Efremova <yefremova@codemastersintl.com>
2024-01-18 07:22:34 +0700

    prod mode enabled

--- angular-chat-app/src/main.ts
+++ angular-chat-app/src/main.ts
@@ -1,5 +1,8 @@
 import {AppComponent} from './app/app.component';
 import {bootstrapApplication} from "@angular/platform-browser";
+import {enableProdMode} from "@angular/core";
+
+enableProdMode();
 
 bootstrapApplication(AppComponent)
   .catch(err => console.error(err));
